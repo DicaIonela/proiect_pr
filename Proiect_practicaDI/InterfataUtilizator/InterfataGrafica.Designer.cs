3dae21adc520f12e89bed4db92bf9d6c8d595322
ioneladd <164873125+ioneladd@users.noreply.github.com>
2024-07-23 07:14:43 -0700

    Add files via upload

---
+++ Proiect_practicaDI/InterfataUtilizator/InterfataGrafica.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace InterfataUtilizator
+{
+    partial class InterfataGrafica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCitire = new System.Windows.Forms.Button();
+            this.btnAfisare = new System.Windows.Forms.Button();
+            this.btnCautare = new System.Windows.Forms.Button();
+            this.btnSterge = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMAC = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(232, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PAGINA PRINCIPALA";
+            // 
+            // btnCitire
+            // 
+            this.btnCitire.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCitire.Location = new System.Drawing.Point(431, 218);
+            this.btnCitire.Name = "btnCitire";
+            this.btnCitire.Size = new System.Drawing.Size(250, 50);
+            this.btnCitire.TabIndex = 1;
+            this.btnCitire.Text = "Citire utilizator";
+            this.btnCitire.UseVisualStyleBackColor = true;
+            this.btnCitire.MouseEnter += new System.EventHandler(this.btnCitire_MouseEnter);
+            this.btnCitire.MouseLeave += new System.EventHandler(this.btnCitire_MouseLeave);
+            // 
+            // btnAfisare
+            // 
+            this.btnAfisare.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAfisare.Location = new System.Drawing.Point(431, 346);
+            this.btnAfisare.Name = "btnAfisare";
+            this.btnAfisare.Size = new System.Drawing.Size(250, 50);
+            this.btnAfisare.TabIndex = 3;
+            this.btnAfisare.Text = "Afisare utilizatori salvati";
+            this.btnAfisare.UseVisualStyleBackColor = true;
+            // 
+            // btnCautare
+            // 
+            this.btnCautare.Font = new System.Drawing.Font("Nirmala UI", 8F);
+            this.btnCautare.Location = new System.Drawing.Point(256, 130);
+            this.btnCautare.Name = "btnCautare";
+            this.btnCautare.Size = new System.Drawing.Size(72, 20);
+            this.btnCautare.TabIndex = 4;
+            this.btnCautare.Text = "Cautare";
+            this.btnCautare.UseVisualStyleBackColor = true;
+            // 
+            // btnSterge
+            // 
+            this.btnSterge.Font = new System.Drawing.Font("Nirmala UI", 10F);
+            this.btnSterge.Location = new System.Drawing.Point(187, 516);
+            this.btnSterge.Name = "btnSterge";
+            this.btnSterge.Size = new System.Drawing.Size(141, 30);
+            this.btnSterge.TabIndex = 6;
+            this.btnSterge.Text = "Sterge utilizator";
+            this.btnSterge.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Coral;
+            this.pictureBox1.BackgroundImage = global::InterfataUtilizator.Properties.Resources.background;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(742, 574);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(437, 557);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Adresa MAC a acestui PC:";
+            // 
+            // lblMAC
+            // 
+            this.lblMAC.AutoSize = true;
+            this.lblMAC.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMAC.Location = new System.Drawing.Point(592, 557);
+            this.lblMAC.Name = "lblMAC";
+            this.lblMAC.Size = new System.Drawing.Size(12, 17);
+            this.lblMAC.TabIndex = 9;
+            this.lblMAC.Text = " ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(238, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 156);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(316, 354);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // InterfataGrafica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 574);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblMAC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSterge);
+            this.Controls.Add(this.btnCautare);
+            this.Controls.Add(this.btnAfisare);
+            this.Controls.Add(this.btnCitire);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "InterfataGrafica";
+            this.Text = "Interfata Grafica";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCitire;
+        private System.Windows.Forms.Button btnAfisare;
+        private System.Windows.Forms.Button btnCautare;
+        private System.Windows.Forms.Button btnSterge;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMAC;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Proiect_practicaDI/InterfataUtilizator/InterfataGrafica.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfataUtilizator
+{
+    public partial class InterfataGrafica : Form
+    {
+        public Size originalSize;
+        public InterfataGrafica()
+        {
+            this.Text = "Interfață Grafică";
+            InitializeComponent();
+            string macAddress = GetMacAddress();
+            // Setează textul pentru Label
+            lblMAC.Text = macAddress;
+        }
+        public static string GetMacAddress()
+        {
+            /*Obtine lista de placi de retea*/
+            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            /*Cautam prima placa de retea activa si obtinem adresa MAC*/
+            foreach (var networkInterface in networkInterfaces)
+            {
+                /*Verifica daca placa de retea nu este de tip Loopback si este activs*/
+                if (networkInterface.NetworkInterfaceType != NetworkInterfaceType.Loopback &&
+                    networkInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    var macAddress = networkInterface.GetPhysicalAddress();
+                    if (macAddress != null)
+                    {
+                        /*Formateaza adresa MAC intr-un sir de caractere hexazecimale*/
+                        return string.Join("::", macAddress.GetAddressBytes().Select(b => b.ToString("X2")));
+                    }
+                }
+            }
+            return "Adresa MAC nu a putut fi gasita";
+        }
+        private void btnCitire_MouseEnter(object sender, EventArgs e)
+        {
+            originalSize = btnCitire.Size;
+            btnCitire.Size = new Size(300, 75);
+        }
+
+        private void btnCitire_MouseLeave(object sender, EventArgs e)
+        {
+            btnCitire.Size = originalSize;
+        }
+    }
+
+}
